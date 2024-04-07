996d628dbb88cabe1891afeeb5c41ef20c16b330
0hllyldz2 <131587192+0hllyldz2@users.noreply.github.com>
2024-04-07 04:23:35 +0300

    Add files via upload

---
+++ 26.-28.Video_KategoriEklemePaneliTasarimiveYeniKategoriEkleme/YemekTarifSitesi/yemektarifsite/KategoriDetay.aspx.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data.SqlClient;
+
+public partial class KategoriDetay : System.Web.UI.Page
+{
+    SQLsinifi bgl = new SQLsinifi();
+    string kategoriid = "";
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        kategoriid = Request.QueryString["Kategoriid"];
+        SqlCommand komut = new SqlCommand("SELECT * FROM Tbl_Yemekler WHERE Kategoriid=@p1", bgl.baglanti());
+        komut.Parameters.AddWithValue("@p1", Convert.ToInt16(kategoriid));
+        SqlDataReader dr = komut.ExecuteReader();
+        DataList2.DataSource = dr;
+        DataList2.DataBind();
+        bgl.baglanti().Close();
+    }
+}
---
+++ 26.-28.Video_KategoriEklemePaneliTasarimiveYeniKategoriEkleme/YemekTarifSitesi/yemektarifsite/Kategoriler.aspx.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data.SqlClient;
+
+public partial class Kategoriler : System.Web.UI.Page
+{
+    SQLsinifi bgl = new SQLsinifi();
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        SqlCommand komut = new SqlCommand("SELECT * FROM Tbl_Kategoriler", bgl.baglanti());
+        SqlDataReader dr = komut.ExecuteReader();
+        DataList1.DataSource = dr;
+        DataList1.DataBind();
+
+        Panel2.Visible = false;
+        Panel4.Visible = false;
+    }
+
+    protected void Button1_Click(object sender, EventArgs e)
+    {
+        Panel2.Visible = true;
+    }
+
+    protected void Button2_Click(object sender, EventArgs e)
+    {
+        Panel2.Visible = false;
+    }
+
+    protected void Button3_Click(object sender, EventArgs e)
+    {
+        Panel4.Visible = true;
+    }
+
+    protected void Button4_Click(object sender, EventArgs e)
+    {
+        Panel4.Visible = false;
+    }
+
+    protected void BtnEkle_Click(object sender, EventArgs e)
+    {
+        SqlCommand komut2 = new SqlCommand("INSERT INTO Tbl_Kategoriler (KategoriAd) VALUES (@p1)", bgl.baglanti());
+        komut2.Parameters.AddWithValue("@p1", TextBox1.Text);
+        komut2.ExecuteNonQuery();
+        bgl.baglanti().Close();
+    }
+}
