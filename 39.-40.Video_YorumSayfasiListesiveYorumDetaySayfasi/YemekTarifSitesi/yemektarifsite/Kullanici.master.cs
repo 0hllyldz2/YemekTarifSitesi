87d979ce157a9bf9a47af45b5e84263f48c25413
0hllyldz2 <131587192+0hllyldz2@users.noreply.github.com>
2024-04-13 14:12:09 +0300

    Add files via upload

---
+++ 39.-40.Video_YorumSayfasiListesiveYorumDetaySayfasi/YemekTarifSitesi/yemektarifsite/Kullanici.master.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data.SqlClient;
+
+public partial class Kullanici : System.Web.UI.MasterPage
+
+{
+    SQLsinifi bgl = new SQLsinifi();
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        SqlCommand komur = new SqlCommand("SELECT * FROM Tbl_Kategoriler", bgl.baglanti());
+        SqlDataReader oku = komur.ExecuteReader();
+        DataList1.DataSource = oku;
+        DataList1.DataBind();
+    }
+}
---
+++ 39.-40.Video_YorumSayfasiListesiveYorumDetaySayfasi/YemekTarifSitesi/yemektarifsite/YorumDetay.aspx.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data.SqlClient;
+
+public partial class YorumDetay : System.Web.UI.Page
+{
+    SQLsinifi bgl = new SQLsinifi();
+    string id;
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        string id = Request.QueryString["Yorumid"];
+
+        if (Page.IsPostBack == false)
+        {
+            SqlCommand komut = new SqlCommand("SELECT YorumAdSoyad, YorumMail, Yorumİcerik, YemekAd FROM Tbl_Yorumlar INNER JOIN Tbl_Yemekler ON Tbl_Yorumlar.Yemekid=Tbl_Yemekler.Yemekid WHERE Yorumid=@p1", bgl.baglanti());
+            if (string.IsNullOrEmpty(id))
+            {
+                komut.Parameters.AddWithValue("@p1", DBNull.Value);
+            }
+            else
+            {
+                komut.Parameters.AddWithValue("@p1", id);
+            }
+            SqlDataReader dr = komut.ExecuteReader();
+            while (dr.Read())
+            {
+                TxtAd.Text = dr[0].ToString();
+                TxtMail.Text = dr[1].ToString();
+                TxtIcerik.Text = dr[2].ToString();
+                TxtYemek.Text = dr[3].ToString();
+            }
+            bgl.baglanti().Close();
+
+        }
+    }
+}
